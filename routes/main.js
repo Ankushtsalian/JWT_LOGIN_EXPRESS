5ce4e5892a949c7db941ce07081e66632a88027e
Ankush <ankushtsalian@gmail.com>
2022-12-10 19:51:07 +0530

    added const authenticationMiddleware to make a protected route(dashboard)

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { route } = require("express/lib/router");
 const router = express.Router();
-
 const { dashboard, login } = require("../controllers/main");
+const authenticationMiddleware = require("../middleware/auth");
 
-router.route("/dashboard").get(dashboard);
+router.route("/dashboard").get(authenticationMiddleware, dashboard);
 router.route("/login").get(login);
 
 module.exports = router;
